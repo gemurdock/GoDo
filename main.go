f3597b08c108e20d24a746397e6d537a6e9f3cb2
Glen Murdock <gemurdock@gmail.com>
2022-07-16 16:23:20 -0400

    Custom middleware - dump request

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
@@ -33,11 +34,33 @@ func main() {
 
 	router.Use(middleware.Logger)
 
+	/*
+		Print out entire request (Middleware)
+	*/
+	router.Use(func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			requestDump, err := httputil.DumpRequest(r, true)
+			if err == nil {
+				fmt.Print("\n\n")
+				fmt.Printf(string(requestDump))
+			}
+			next.ServeHTTP(w, r)
+		}
+
+		return http.HandlerFunc(fn)
+	})
+
+	/*
+		Serve ping.html
+	*/
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		responseCount.Count += 1
 
 		compiledTemplates.ExecuteTemplate(w, "ping.html", responseCount)
 	})
 
+	/*
+		Start server
+	*/
 	http.ListenAndServe(":80", router)
 }
